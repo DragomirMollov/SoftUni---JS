da403088cae20970f714a2f96de035311b5255fc
DragomirMollov <44067945+DragomirMollov@users.noreply.github.com>
2022-06-11 13:19:16 +0100

    Create 02 Add and Subtract.js

---
+++ JavaSript/Fundamentals/Functions - Exercise/02 Add and Subtract.js
@@ -0,0 +1,7 @@
+function smallestOfThreeNumbers(first, second, last) {
+    
+    let smallestNumber = Math.min(first, second, last);
+    return smallestNumber
+}
+
+console.log(smallestOfThreeNumbers(2, 5, 3));
