8f8cce0511598a18881c6516e466dce85b3e29d8
DragomirMollov <44067945+DragomirMollov@users.noreply.github.com>
2022-06-11 13:26:50 +0100

    Delete JavaSript/Fundamentals/Functions - Exercise directory

--- JavaSript/Fundamentals/Functions - Exercise/02 Add and Subtract.js
+++
@@ -1,7 +0,0 @@
-function smallestOfThreeNumbers(first, second, last) {
-    
-    let smallestNumber = Math.min(first, second, last);
-    return smallestNumber
-}
-
-console.log(smallestOfThreeNumbers(2, 5, 3));
